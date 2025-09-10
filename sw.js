84fcb5709cac637f758c36562d8045465cba8cc8
Cross Nastasi <cross@dill.moe>
2025-09-09 23:43:22 -0400

    Fix noise break issue

--- sw.js
+++ sw.js
@@ -1,7 +1,11 @@
-const CACHE_NAME = 'white-noise-cache-v1';
+const CACHE_NAME = 'white-noise-cache-v2';
 const urlsToCache = [
   '/',
-  '/index.html'
+  '/index.html',
+  '/noise-processor.js',
+  '/all.min.css',
+  '/favicon.png',
+  '/webfonts/fa-solid-900.woff2'
 ];
 
 self.addEventListener('install', event => {
@@ -11,6 +15,21 @@ self.addEventListener('install', event => {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
 
